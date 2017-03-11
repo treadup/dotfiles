3ed9b7c4393aa7b02c64dc4b8681f77f5ff73fa3
Henrik Treadup <henrik.treadup@gmail.com>
2017-03-11 16:48:32 +0100

    Install packages as part of setting up a new machine.

--- computer/setup-new-machine.sh
+++ computer/setup-new-machine.sh
@@ -12,13 +12,76 @@ touch ~/.already_configured
 # Install packages.
 apt-get --force-yes --yes update
 apt-get --force-yes --yes upgrade
-apt-get --force-yes --yes install language-pack-en pass git emacs24-nox vim trash-cli npm \
-        httpie virtualenv silversearcher-ag xzdec screen whois pwgen \
-        python3-dev python3-pip python3-setuptools \
-        awscli pandoc texlive-full \
-        heroku certbot \
-        docker
-apt-get --force-yes -yes autoremove
+
+# Install english language pack
+apt-get --force-yes --yes install language-pack-en
+
+# Install git
+apt-get --force-yes --yes install git
+
+# Install password utilities
+apt-get --force-yes --yes install pass pwgen
+
+# Install editors
+apt-get --force-yes --yes install emacs24-nox vim
+
+# Install cli trash command
+apt-get --force-yes --yes install trash-cli
+
+# Install node and npm
+apt-get --force-yes --yes install nodejs npm
+
+# Install texlive
+# For some reason xzdec is needed to get tlmgr to work
+apt-get --force-yes --yes install texlive-full xzdec
+
+# Install http cli clients
+apt-get --force-yes --yes install curl wget httpie
+
+# Install ag
+apt-get --force-yes --yes install silversearcher-ag
+
+# Install screen
+apt-get --force-yes --yes install screen
+
+# Install Python related things
+apt-get --force-yes --yes install python3-pip virtualenv python3-dev
+
+# Install network tools
+apt-get --force-yes --yes install whois
+
+# Install pandoc
+apt-get --force-yes --yes install pandoc
+
+# Install heroku
+apt-get --force-yes --yes install heroku
+
+# Install certbot
+apt-get --force-yes --yes install certbot
+
+# Install aws cli command
+apt-get --force-yes --yes install awscli
+
+# Install GCC and make among other things
+apt-get --force-yes --yes install build-essential
+
+# Install docker
+apt-get --force-yes --yes install docker.io
+
+# Install Java8 SDK
+apt-get --force-yes --yes install default-jdk
+
+# Install Steel Bank Common Lisp
+apt-get --force-yes --yes install sbcl
+
+# Install Gambit Scheme
+apt-get --force-yes --yes install gambc
+
+# Install Go
+apt-get --force-yes --yes install golang
+
+# Remove pacakges that are no longer needed
+apt-get --force-yes --yes autoremove
 
 # Setup the dot files from git repo
 # The following is to get around the fact that we cannot clone into a non empty directory.
@@ -34,3 +97,14 @@ git clone git@github.com:treadup/.emacs.d.git
 tlmgr -v init-usertree
 tlmgr install moderncv
 
+# Create the bin folder.
+mkdir -p ~/bin
+
+# Install Clojure
+curl -o ~/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod 755 ~/bin/lein
+~/bin/lein
+
+# Setup the password store
+echo Remember that you now have to setup gpg and pass
+echo To set up pass remember to use pass git init instead of pass init
