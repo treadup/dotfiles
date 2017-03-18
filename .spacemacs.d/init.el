d14d0f2fe5096916495f983aa3fd56c7a5778b59
Henrik Treadup <henrik.treadup@gmail.com>
2017-03-18 12:40:14 +0100

    The spacemacs init file.

---
+++ .spacemacs.d/init.el
@@ -0,0 +1,7 @@
+;; This is a placeholder for the spacemacs configuration file.
+;; Replace this file with the .spacemacs file that the Spacemacs
+;; installer generates.
+
+;; I prefer to have the setup file in .spacemacs.d/init.el 
+;; instead of in the file .spacemacs.
+
