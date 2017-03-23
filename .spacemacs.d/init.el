a729d1982136dbce48e88835d6324b558d67cf0f
Henrik Treadup <henrik.treadup@gmail.com>
2017-03-23 09:25:25 +0100

    Added Spacemacs sql layer.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -85,6 +85,7 @@ values."
      ruby
      scheme
      shell-scripts
+     sql
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
