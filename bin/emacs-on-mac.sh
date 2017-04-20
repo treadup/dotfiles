41cf687031a244f599e87bc039d96ed00bfa8435
Henrik Treadup <henrik.treadup@gmail.com>
2017-04-20 13:51:30 +0200

    Emacs on Mac.

---
+++ bin/emacs-on-mac.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This script is so that we can execute the latest version of Emacs from the command line.
+# Without this script when you execute the emacs command it will start an older version of Emacs.
+
+# Create a symlink from ~/bin/emacs to this file.
+
+/Applications/Emacs.app/Contents/MacOS/Emacs "$@"
+
