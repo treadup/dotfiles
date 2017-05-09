0e78220c985a3b8d9cda1434f3e2ea24a29ffbe2
Henrik Treadup <henrik.treadup@gmail.com>
2017-05-09 23:23:35 +0200

    Switch from apt-get to apt. Install heroku.

--- computer/install-ubuntu-packages.sh
+++ computer/install-ubuntu-packages.sh
@@ -1,85 +1,89 @@
 #!/bin/bash
 
 # Install packages.
-apt-get --yes update
-apt-get --yes upgrade
+apt --yes update
+apt --yes upgrade
 
 # Install english language pack
-apt-get --yes install language-pack-en
+apt --yes install language-pack-en
 
 # Install git
-apt-get --yes install git
+apt --yes install git
 
 # Install password utilities
-apt-get --yes install pass pwgen
+apt --yes install pass pwgen
 
 # Install Emacs
 if [ -z $DISPLAY ]; then
-    apt-get --yes install emacs-nox
+    apt --yes install emacs-nox
 else
-    apt-get --yes install emacs
+    apt --yes install emacs
 fi
 
 # Install Vim
-apt-get --yes install vim
+apt --yes install vim
 
 # Install cli trash command
-apt-get --yes install trash-cli
+apt --yes install trash-cli
 
 # Install ubuntu make
-apt-get --yes install ubuntu-make
+apt --yes install ubuntu-make
 
 # Install zip
-apt-get --yes install zip unzip
+apt --yes install zip unzip
 
 # Install node and npm
 # The node ecosystem expects the node executable to be called node.
 # Therefore create a symlink to the correct name.
-apt-get --yes install nodejs npm
+apt --yes install nodejs npm
 ln -s /usr/bin/nodejs /usr/local/bin/node
 
 # Install texlive
 # For some reason xzdec is needed to get tlmgr to work
-apt-get --yes install texlive-full xzdec
+apt --yes install texlive-full xzdec
 
 # Install http cli clients
-apt-get --yes install curl wget httpie
+apt --yes install curl wget httpie
 
 # Install ag
-apt-get --yes install silversearcher-ag
+apt --yes install silversearcher-ag
 
 # Install screen
-apt-get --yes install screen
+apt --yes install screen
 
 # Install Python related things
-apt-get --yes install python3-pip virtualenv python3-dev
+apt --yes install python3-pip virtualenv python3-dev
 
 # Install network tools
-apt-get --yes install whois
+apt --yes install whois
 
 # Install pandoc
-apt-get --yes install pandoc
+apt --yes install pandoc
 
 # Install heroku
-apt-get --yes install heroku
+## Add repo for heroku cli program
+add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+apt update 
+apt --yes install heroku
 
 # Install certbot
-apt-get --yes install certbot
+apt --yes install certbot
 
 # Install aws cli command
-apt-get --yes install awscli
+apt --yes install awscli
 
 # Install GCC and make among other things
-apt-get --yes install build-essential
+apt --yes install build-essential
 
 ## Install LLVM and CLang
-apt-get --yes install llvm clang
+apt --yes install llvm clang
 
 # Install docker
-apt-get --yes install docker.io
+apt --yes install docker.io
 
 # Install Java8 SDK
-apt-get --yes install default-jdk
+apt --yes install default-jdk
 
 # Set the JAVA_HOME environment variable. You need to log out and
 # then log back in again for the /etc/environment file to be reloaded.
@@ -87,22 +91,22 @@ JAVA_HOME="$(dirname $(dirname $(readlink -e $(which javac))))"
 printf "JAVA_HOME=$JAVA_HOME\n" >> /etc/environment 
 
 # Install Steel Bank Common Lisp
-apt-get --yes install sbcl
+apt --yes install sbcl
 
 # Install Gambit Scheme
-apt-get --yes install gambc
+apt --yes install gambc
 
 # Install MIT Scheme
-apt-get --yes install mit-scheme
+apt --yes install mit-scheme
 
 # Install Go
-apt-get --yes install golang
+apt --yes install golang
 
 # Install Guile Scheme
-apt-get --yes install guile-2.0
+apt --yes install guile-2.0
 
 # Install weechat
-apt-get --yes install weechat
+apt --yes install weechat
 
 # Remove packages that are no longer needed
-apt-get --yes autoremove
+apt --yes autoremove
