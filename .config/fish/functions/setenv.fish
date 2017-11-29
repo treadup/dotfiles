4e163299419200e15cffc40b5406070fdab615db
Henrik Treadup <henrik.treadup@gmail.com>
2017-11-29 21:07:50 +0100

    Use $HOME in defintion of PATH env var instead of ~/. Do not use VIRTUALENV_PYTHON since it breaks tools like pew and vex.

--- .config/fish/functions/setenv.fish
+++ .config/fish/functions/setenv.fish
@@ -3,10 +3,8 @@ function setenv
     # Split the line on colons and spaces
     for item in (echo $argv[2] | tr ': ' \n)
       if [ $item != "\$PATH" ]
-        # Force full expansion of the item variable.
-        set -l expanded_item (eval echo (eval echo "$item"))
-        if not contains $expanded_item $PATH
-          set -g PATH $PATH $expanded_item
+        if not contains $item $PATH
+          set -g PATH $PATH (eval echo $item)
         end
       end
     end
