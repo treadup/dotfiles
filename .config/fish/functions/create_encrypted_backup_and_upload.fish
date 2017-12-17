15c4e360c1b740a1ef46ff7f38f757fba8d4a5a8
Henrik Treadup <henrik.treadup@gmail.com>
2017-12-17 02:54:39 +0100

    Create encrypted backup and upload.

---
+++ .config/fish/functions/create_encrypted_backup_and_upload.fish
@@ -0,0 +1,20 @@
+function create_encrypted_backup
+  set -l BACKUP_FILENAME /backup/backup_(hostname)_(date +"%Y%m%d_%H%M").tgz.gpg
+  cd $HOME
+  tar -cpz \
+  --exclude './Downloads' \
+  --exclude './Videos' \
+  --exclude './VirtualBox VMs' \
+  --exclude './Music' \
+  --exclude './Desktop' \
+  --exclude './Private' \
+  --exclude './private' \
+  --exclude './.*' \
+  --exclude './Linux' \
+  --exclude './linux' \
+  --exclude './tmp' \
+  --exclude './node_modules' \
+  --exclude './nerd_fonts' \
+  . | gpg --output $BACKUP_FILENAME --encrypt --recipient 'henrik.treadup@gmail.com'
+aws s3 cp $BACKUP_FILENAME s3://redwood-backup/
+end
