0b57d8a77e900cdf55155fae92950428440961cf
Henrik Treadup <henrik.treadup@gmail.com>
2018-02-12 23:01:56 +0100

    Simple prompt for dumb terminals.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,34 +1,39 @@
 function fish_prompt
-   # Conda does not use the virtualenv variable.
-   if test -n "$CONDA_DEFAULT_ENV"
-      set_color cyan
-      printf '%s ' (basename $CONDA_DEFAULT_ENV)
-      set_color normal
-   end
+    # TRAMP does not work with a fancy prompt.
+    # Therefore supply a prompt for dumb terminals.
+    if test $TERM = "dumb"
+        echo "\$ "
+    else
+        # Conda does not use the virtualenv variable.
+        if test -n "$CONDA_DEFAULT_ENV"
+            set_color cyan
+            printf '%s ' (basename $CONDA_DEFAULT_ENV)
+            set_color normal
+        end
 
-   if test -n "$VIRTUAL_ENV"
-       set_color cyan
-       printf '%s ' (basename $VIRTUAL_ENV)
-       set_color normal
-   end
+        if test -n "$VIRTUAL_ENV"
+            set_color cyan
+            printf '%s ' (basename $VIRTUAL_ENV)
+            set_color normal
+        end
 
-   set -l prompt_git_branch (git branch 2>/dev/null | grep '^*' | colrm 1 2)
+        set -l prompt_git_branch (git branch 2>/dev/null | grep '^*' | colrm 1 2)
 
-   set_color green
-   printf "%s@%s " (whoami) (prompt_hostname)
-   set_color normal
+        set_color green
+        printf "%s@%s " (whoami) (prompt_hostname)
+        set_color normal
 
-   if test -n "$prompt_git_branch"
-      set_color magenta
-      printf "%s " $prompt_git_branch
-      set_color normal
-   end
+        if test -n "$prompt_git_branch"
+            set_color magenta
+            printf "%s " $prompt_git_branch
+            set_color normal
+        end
 
-   set_color white
-   printf "%s " (prompt_pwd)
-   set_color normal
+        set_color white
+        printf "%s " (prompt_pwd)
+        set_color normal
 
-   # Lowercase lambda on new line
-   printf "\n\u03BB "
+        # Lowercase lambda on new line
+        printf "\n\u03BB "
+    end
 end
-
