62fc43ed52d870c2e7cd368125b1af9854dabee1
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-05 19:25:48 +0200

    Remove StumpWM init file at .stumpwm.d/init.lisp since it was not being read.

--- .stumpwm.d/init.lisp
+++
@@ -1,8 +0,0 @@
-;; This is the user configuration file for StumpVM.
-
-(in-package :stumpwm)
-
-;; Map the CapsLock key to Ctrl
-(setq *caps-lock-behavior* :ctrl)
-
-;; Ctrl-Alt-Backspace should kill Xorg.
