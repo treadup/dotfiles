f8dccb0a362965aeb2ee4bfedf86009d1391ab3a
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-08 22:55:45 +0200

    Fish function for opening emacs in the termianl.

---
+++ .config/fish/functions/et.fish
@@ -0,0 +1,24 @@
+
+# The et function opens the given arguments in Emacs
+# in the terminal.
+
+# Currently this script only works if you are running Emacs
+# in a window-system. It will break things if you try to run
+# it from the terminal.
+
+# The values that make sense for the alternate editor are
+# emacs and "".
+
+# When you use emacs it will start a new instance of emacs.
+# This will in turn start the server.
+
+# When you use the empty string as the alternate editor and
+# you are unable to connect start emacs in daemon mode and
+# try to connect again.
+
+function et
+    # -t         Open in the current terminal
+    # -q         Show no messages on success
+    # -a emacs   If the emacs server cannot be found run emacs.
+    emacsclient -t -q -a emacs $argv
+end
