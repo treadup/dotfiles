30ee0054fb9e8cb295708a0794166519fb8113ba
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-24 15:14:15 +0200

    Do not install OpenBox or MATE desktop environment.

--- computer/ubuntu/install-ubuntu-packages.sh
+++ computer/ubuntu/install-ubuntu-packages.sh
@@ -155,12 +155,6 @@ apt install -y fonts-powerline
 # Install stumpwm using quicklisp instead.
 # apt install -y stumpwm
 
-# Install OpenBox
-apt install -y openbox
-
-# Install MATE desktop environment
-apt install -y mate-desktop-environment-extras
-
 #
 # Install flatpak
 #
