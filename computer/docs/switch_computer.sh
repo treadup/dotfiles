78b4db4fac942465b8f3833606b8327fc15cac76
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-29 22:11:57 +0200

    Switch computer.

---
+++ computer/docs/switch_computer.sh
@@ -0,0 +1,10 @@
+# Switch Computer
+The following are some things to think about when switching to a new
+computer.
+
+## GPG keys
+Make sure to take backups of the GPG keys.
+
+## SSH keys
+Make sure to take backups of the SSH keys. These are found in the .ssh
+folder. In fact it is a good idea to backup the entire .ssh folder.
