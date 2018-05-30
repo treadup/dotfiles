e2c80dad4ea21808a436e36a8e6c85df78fae9ba
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-30 16:58:42 +0200

    Document switching computer.

--- computer/docs/switch_computer.sh
+++ computer/docs/switch_computer.sh
@@ -8,3 +8,17 @@ Make sure to take backups of the GPG keys.
 ## SSH keys
 Make sure to take backups of the SSH keys. These are found in the .ssh
 folder. In fact it is a good idea to backup the entire .ssh folder.
+
+## Code
+Make sure that for each of the repos you have commited and pushed all
+the code that you want to keep.
+
+## Files
+Make sure that you take a backup of all the files that you want to keep.
+
+## Config
+Make sure that the config is checked in and that you have saved all files
+that you want to save.
+
+## Bookmarks and links
+Take a backup of the bookmarks and links from the browser.
