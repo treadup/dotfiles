76d169d85a5c45e096073a26d868528d51b71562
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-31 15:15:09 +0200

    Updated lockscreen command for Linux.

--- .config/fish/functions/lockscreen.fish
+++ .config/fish/functions/lockscreen.fish
@@ -1,4 +1,9 @@
 function lockscreen
-    echo "Ctrl-Cmd-Q to lock the screen."
-    /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+    switch (uname)
+        case Linux
+	    light-locker-command -l
+        case Darwin
+            echo "Ctrl-Cmd-Q to lock the screen."
+            /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+    end
 end
