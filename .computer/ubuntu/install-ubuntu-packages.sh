2d9dbae1c089fc623c9acbfde4f68fe60ad0f668
Henrik Treadup <henrik.treadup@gmail.com>
2018-06-04 14:14:53 +0200

    Install xdotoll.

--- .computer/ubuntu/install-ubuntu-packages.sh
+++ .computer/ubuntu/install-ubuntu-packages.sh
@@ -175,6 +175,9 @@ apt install -y openbox
 # Install wmctrl. This is needed for my run-and-raise script.
 apt install -y wmctrl
 
+# Install xdotool
+apt install -y xdotool
+
 # I use feh to display a backround image under X.
 apt install -y feh
 
