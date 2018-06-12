841f362748fa76a69debe10e668ff4de760a7f5f
Henrik Treadup <henrik.treadup@gmail.com>
2018-06-12 11:36:35 +0200

    Empty hammerspoon config file.

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,8 @@
+
+--[[
+Hammerspoon is a tool for automation of macOS. It can be used to create
+keyboard shortcuts to launch applications and to replace window managers
+like Spectacle.
+
+https://www.hammerspoon.org/
+--]]
