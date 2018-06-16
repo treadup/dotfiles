83c4a43f8cab55ec8c78775fab46fa58b6d5ccca
Henrik Treadup <henrik.treadup@gmail.com>
2018-06-16 14:17:27 +0200

    Move OpenBox installation to separate script.

--- .computer/ubuntu/install-ubuntu-packages.sh
+++ .computer/ubuntu/install-ubuntu-packages.sh
@@ -199,30 +199,6 @@ apt install -y gnome-shell-extension-dashtodock
 apt install -y gnome-shell-extension-caffeine
 apt install -y gnome-shell-extension-move-clock
 
-# Install OpenBox
-apt install -y openbox
-
-# Install wmctrl. This is needed for my run-and-raise script.
-apt install -y wmctrl
-
-# Install xdotool
-apt install -y xdotool
-
-# I use feh to display a backround image under X.
-apt install -y feh
-
-# Install light-locker
-apt install -y light-locker
-
-# Install arandr
-apt install -y arandr
-
-# Volume control applet
-apt install -y pasystray
-
-# Panel
-apt install -y tint2
-
 #
 # Install flatpak
 #
