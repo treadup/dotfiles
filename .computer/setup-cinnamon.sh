9476ac23896d6077bc0f1faf3fd71a97d1fe9c33
Henrik Treadup <henrik.treadup@gmail.com>
2018-06-25 18:04:03 +0200

    Setup script for Cinnamon.

---
+++ .computer/setup-cinnamon.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Cinnamon is configured using the System Settings application.
+# To see the changes that are being made you can use the command
+# dconf watch /
+
+# Cinnamon stores its configuration settings in the org.cinnamon
+# schema.
+
+#
+# Fonts
+#
+gsettings set org.cinnamon.desktop.interface font-name 'Sans 12'
+gsettings set org.nemo.desktop font 'Noto Sans 13'
+gsettings set org.gnome.desktop.interface document-font-name 'Sans 14'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono 15'
+gsettings set org.cinnamon.desktop.wm.preferences titlebar-font 'Sans Bold 12'
+
+#
+# Background image
+#
+if [ -f ~/Pictures/rainforest_bridge.png ]; then
+    BACKGROUND_PATH="file://$(realpath ~/Pictures/rainforest_bridge.png)"
+    gsettings set org.cinnamon.desktop.background picture-uri $BACKGROUND_PATH
+fi
+
+#
+# Workspaces
+#
+gsettings set org.cinnamon.desktop.wm.preferences num-workspaces 9
+
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-1 "['<Super>1']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-2 "['<Super>2']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-3 "['<Super>3']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-4 "['<Super>4']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-5 "['<Super>5']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-6 "['<Super>6']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-7 "['<Super>7']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-8 "['<Super>8']"
+gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-9 "['<Super>9']"
+
+#
+# Custom key bindings
+#
+
+
+
+#
+# Keyboard
+#
+# gsettings set org.gnome.libgnomekbd.keyboard options "['ctrl\tctrl:nocaps']"
+gsettings set org.gnome.libgnomekbd.keyboard options "['ctrl\tctrl:nocaps', 'grp\tgrp:win_space_toggle']"
+
+#
+# Panel
+#
+
+# Move the panel to the top of the screen.
+gsettings set org.cinnamon panels-enabled "['1:0:top']"
