d833b803a574165cdf7e8632cefcf736567cc315
Henrik Treadup <henrik.treadup@gmail.com>
2019-07-16 12:34:20 +0200

    Install godoc

--- .computer/setup-computer.sh
+++ .computer/setup-computer.sh
@@ -168,6 +168,7 @@ go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/guru
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v golang.org/x/tools/cmd/godoc
 go get -u -v github.com/motemen/gore
 
 # Install ppd
