843bbc661789cf931b49128c421d79e1d8f03232
Henrik Treadup <henrik.treadup@gmail.com>
2019-09-09 21:49:26 +0200

    Install SDL 1.2 on Ubuntu

--- .computer/ubuntu/install-ubuntu-packages.sh
+++ .computer/ubuntu/install-ubuntu-packages.sh
@@ -181,6 +181,13 @@ apt-get install -y fzy
 apt-get install -y pick
 
 #
+# Graphics
+#
+
+# Install SDL 1.2
+apt-get install -y libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
+
+#
 # Logging
 #
 
