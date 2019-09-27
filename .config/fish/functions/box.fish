29d27c616c24eedee8ea371a2b59e025555b198d
Henrik Treadup <henrik.treadup@gmail.com>
2019-09-27 11:03:11 +0200

    Fish function for working with projects

---
+++ .config/fish/functions/box.fish
@@ -0,0 +1,82 @@
+function box
+    switch "$argv[1]"
+	case "db"
+	    _box_db $argv[2..-1]
+	case "start"
+	    _box_start $argv[2..-1]
+	case "deploy"
+	    _box_deploy $argv[2..-1]
+	case ""
+	    echo "Usage: box <command>"
+	    echo "where <command> can be one of the following."
+	    echo "db     - perform database commands"
+	    echo "deploy - perform deploy commands"
+	    echo "start  - perform start commands"
+	case "*"
+	    # echo "Unknown command: $argv[1]"
+	    eval "cd ~/work/flowbox/ && make $argv"
+    end
+end
+
+
+function _box_start
+    switch "$argv[1]"
+	case "frontend"
+	    eval "cd ~/work/frontend && yarn check --integrity && yarn start"
+	case "backend"
+	    eval "cd ~/work/flowbox && docker-compose up flask-app"
+	case ""
+	    echo "Usage: box start <command>"
+	    echo "where <command> can be one of the following."
+	    echo "frontend - start frontend application"
+	    echo "backend  - start backend application"
+	case "*"
+	    echo "Unknown start command: $argv[1]"
+    end
+end
+
+
+function _box_deploy
+    switch "$argv[1]"
+	case "celerytest"
+	    echo "Deploying celery to test"
+	    eval "cd ~/work/flowbox/flask-app/ && fab test celery deploy"
+	case "celeryprod"
+	    echo "Deploying celery to production"
+	    eval "cd ~/work/flowbox/flask-app/ && fab live celery deploy"
+	case "webtest"
+	    echo "Deploying web to test"
+	    eval "cd ~/work/flowbox/flask-app/ && fab test web deploy "
+	case "webprod"
+	    echo "Deploying web to production"
+	    eval "cd ~/work/flowbox/flask-app/ && fab live web deploy "
+	case ""
+	    echo "Usage: box deploy <command>"
+	    echo "where <command> can be one of the following."
+	    echo "celerytest - deploy celery to test"
+	    echo "celeryprod - deploy celery to prod"
+	    echo "webtest    - deploy web to test"
+	    echo "webprod    - deploy web to prod"
+	case "*"
+	    echo "Unknown deploy command: $argv[1]"
+    end
+end
+
+
+function _box_db
+    switch "$argv[1]"
+	case "psql"
+	    _box_db_psql $argv[2..-1]
+	case "upgrade"
+	    eval "cd ~/work/flowbox/ && docker-compose exec flask-app python manage.py db upgrade"
+	case "downgrade"
+	    eval "cd ~/work/flowbox/ && docker-compose exec flask-app python manage.py db downgrade"
+	case ""
+	    echo "Usage: box db <command>"
+	    echo "where <command> can be one of the following."
+	    echo "psqltest - connect to the test database"
+	    echo "pdqlprod - connect to the production database"
+	case "*"
+	    echo "Unknown db command: $argv[1]"
+    end
+end
