feb8f00af0bb9da8e3b176d018c269f81bb4e31a
Henrik Treadup <henrik.treadup@gmail.com>
2021-09-25 17:58:23 +0200

    Vultr boot script

---
+++ .computer/ubuntu/vultr_boot_script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Setup standard Ubuntu server using dotfiles setup script
+curl -O https://raw.githubusercontent.com/treadup/dotfiles/master/.computer/ubuntu/setup-ubuntu.sh
+bash setup-ubuntu.sh
+
+
