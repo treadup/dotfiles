858248b63e0167b94ac3d7f8583ce9fa1021326d
Henrik Treadup <henrik.treadup@gmail.com>
2021-09-26 12:36:24 +0200

    Install GitHub CLI tool
    Install gh cli tool.

--- .computer/ubuntu/setup-ubuntu.sh
+++ .computer/ubuntu/setup-ubuntu.sh
@@ -565,6 +565,16 @@ cd /home/henrik
 sudo -u henrik fish -c 'vf install'
 cd /root
 
+echo -------------------------------------
+echo Install GitHub CLI tool
+echo -------------------------------------
+
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 echo ---------------------------------
 echo End notes
 echo ---------------------------------
