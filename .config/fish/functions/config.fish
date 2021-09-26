f4f2540e0951bcfdb17de6ce39aec4b6ec4dff61
Henrik Treadup <henrik.treadup@gmail.com>
2021-09-26 14:11:04 +0200

    Change config command not to be runnable inside a git repo

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,9 @@
 function config
-    /usr/bin/git --git-dir=$HOME/.dotconf/ --work-tree=$HOME $argv
+    if git status &> /dev/null
+        echo "The config command has been disabled inside a git repo."
+        echo "Did you mean to use the git command?"
+        return 1
+    else
+        /usr/bin/git --git-dir=$HOME/.dotconf/ --work-tree=$HOME $argv
+    end
 end
