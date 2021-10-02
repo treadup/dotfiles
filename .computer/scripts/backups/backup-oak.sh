811e2234abb536bc60d660311446ebfd8cf922a7
Henrik Treadup <henrik.treadup@gmail.com>
2021-10-02 19:20:40 +0200

    Fix backup script to correctly copy over oak_backup_exclude.txt

--- .computer/scripts/backups/backup-oak.sh
+++ .computer/scripts/backups/backup-oak.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -u
+
 COMPUTER="henrik@oak.mountainrainforest.org"
 TAR_COMMAND="tar -X .oak_backup_exclude  -czvf - . 2> ~/.sshbackup.log"
 
@@ -7,6 +10,6 @@ BACKUP_DIR=/backups/oak
 mkdir -p $BACKUP_DIR
 BACKUP_FILENAME="$BACKUP_DIR/oak$(date +%Y%m%d-%H%M).tar.gz"
 echo Copying over exclude file
-scp ~/.computer/scripts/oak_backup_exclude.txt $COMPUTER:~/.oak_backup_exclude
-echo Creating backup
+scp ~/.computer/scripts/backups/oak_backup_exclude.txt $COMPUTER:~/.oak_backup_exclude
+echo "Creating backup $BACKUP_FILENAME"
 ssh $COMPUTER "$TAR_COMMAND" > $BACKUP_FILENAME
