69347740789a4c1e18ac979052718c41aacbf773
Henrik Treadup <henrik.treadup@gmail.com>
2021-10-05 17:53:27 +0200

    Change to Linux home folder location
    The ~ was expanded using the Macintosh rules which is not what we want.

--- .config/fish/functions/upload.fish
+++ .config/fish/functions/upload.fish
@@ -1,6 +1,6 @@
 function upload -d "Uploads a file to oak"
     for f in $argv
-	ssh henrik@oak.mountainrainforest.org mkdir -p ~/transfer
+	ssh henrik@oak.mountainrainforest.org mkdir -p /home/henrik/transfer
 	scp $f henrik@oak.mountainrainforest.org:~/transfer/(basename $f)
     end
 end
