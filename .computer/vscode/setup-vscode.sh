6a0780daf9ad4576e55af355abdc62d21eddb2ae
Henrik Treadup <henrik.treadup@gmail.com>
2021-10-14 20:47:53 +0200

    Script to setup VSCode

---
+++ .computer/vscode/setup-vscode.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# To list all of the currently installed extensions you can use the following command.
+# code --list-extensions
+
+# https://marketplace.visualstudio.com/items?itemName=ms-python.python
+code --install-extension ms-python.python
+
+# https://marketplace.visualstudio.com/items?itemName=tootone.org-mode
+code --install-extension tootone.org-mode
+
+# https://marketplace.visualstudio.com/items?itemName=bmalehorn.vscode-fish
+code --install-extension bmalehorn.vscode-fish
+
+# https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-docker
+code --install-extension ms-azuretools.vscode-docker
+
