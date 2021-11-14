198a11208ce532837d2db0bd5c7c3f31c7df41fa
Henrik Treadup <henrik.treadup@gmail.com>
2021-11-14 14:51:02 +0100

    Enforce branch names in the g git wrapper function

--- .config/fish/functions/g.fish
+++ .config/fish/functions/g.fish
@@ -1,5 +1,20 @@
 function g --wraps git
     # Using --wraps will give us the completions for the git command
     # when using the new g function.
-        git $argv;
+    if test "$argv[1]" = "new-branch"
+        switch $argv[2]
+        case 'bugfix/*'
+            echo "Creating bugfix/ branch"
+        case 'feature/*'
+            echo "Creating feature/ branch"
+        case 'tech/*'
+            echo "Creating tech/ branch"
+        case ''
+            echo "Missing branch name"
+        case '*'
+            echo "Branch names should start with bugfix/, feature/ or tech/."
+            return 1
+        end
+    end
+    git $argv;
 end
