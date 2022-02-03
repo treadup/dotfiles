6ba52e683907d025b23d35ecdf7ba1dc108d0811
Henrik Treadup <henrik.treadup@gmail.com>
2022-02-03 09:43:11 +0100

    Block serving of special folders
    Block serving the home folder, the .ssh folder and
    the .gnupg folder.

--- .config/fish/functions/serve.fish
+++ .config/fish/functions/serve.fish
@@ -1,7 +1,22 @@
 function serve
-    set workingdir (pwd)
     set currentip (localip)
     set port 6060
-    echo "Serving $workingdir on $currentip:$port"
+
+    if [ "$PWD" = "$HOME" ]
+        echo "Blocking serving of home directory"
+        return 1
+    end
+
+    if [ "$PWD" = "$HOME/.ssh" ]
+        echo "Blocking serving of .ssh folder"
+        return 1
+    end
+
+    if [ "$PWD" = "$HOME/.gnupg" ]
+        echo "Blocking serving of .gnupg folder"
+        return 1
+    end
+
+    echo "Serving $PWD on $currentip:$port"
     python3 -m http.server $port > /dev/null
 end
