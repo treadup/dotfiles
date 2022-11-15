7abe38346369582df1a65c70de0173bc18d7d5b5
Henrik Treadup <henrik.treadup@gmail.com>
2022-11-15 22:05:08 +0100

    Slow things down a bit in the tab command
    Use three osascript commands instead of one to slow things down a bit.
    This seems to solve the issue where the command is printed twice.

--- .config/fish/functions/tab.fish
+++ .config/fish/functions/tab.fish
@@ -1,6 +1,6 @@
 # The tab function runs the given command in a new tab.
 function tab
-    osascript -e 'tell application "Terminal" to activate' \
-              -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
-              -e "tell application \"Terminal\" to do script \"$argv\" in selected tab of the front window"
+    osascript -e 'tell application "Terminal" to activate'
+    osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' 
+    osascript -e "tell application \"Terminal\" to do script \"$argv\" in selected tab of the front window"
 end
