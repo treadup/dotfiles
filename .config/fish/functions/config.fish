50584d3edda3144274c2b139e8b1ae3117783c51
Henrik Treadup <henrik.treadup@gmail.com>
2024-01-27 14:12:45 +0100

    Remove fish config function
    There is a new fish function called dotfiles that should be used
    for version controlling the dotfiles.

--- .config/fish/functions/config.fish
+++
@@ -1,9 +0,0 @@
-function config
-    if git status &> /dev/null
-        echo "The config command has been disabled inside a git repo."
-        echo "Did you mean to use the git command?"
-        return 1
-    else
-        /usr/bin/git --git-dir=$HOME/.dotconf/ --work-tree=$HOME $argv
-    end
-end
