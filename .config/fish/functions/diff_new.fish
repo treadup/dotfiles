b6aafcfc67b5713abfae600456b4a7bccb4231d3
Henrik Treadup <henrik.treadup@gmail.com>
2024-07-12 08:42:50 +0200

    Stubs for diff_new and diff_old

---
+++ .config/fish/functions/diff_new.fish
@@ -0,0 +1,9 @@
+# Keep lines that do not start with -
+#
+# If line starts with + print it in green otherwise print it in white.
+#
+# You have to use the -U1000 option with git for this to work well.
+#
+# This should be implemented in Python and not in fish.
+#
+# git diff -U1000
---
+++ .config/fish/functions/diff_old.fish
@@ -0,0 +1,9 @@
+# Keep lines that do not start with +
+#
+# If line starts with - print it in red otherwise print it in white.
+#
+# You have to use the -U1000 option with git for this to work well.
+#
+# This should be implemented in python and not in fish.
+#
+# git diff -U1000
